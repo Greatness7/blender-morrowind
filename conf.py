4e379591e7a9f38930fae55b59cf426a47b18df5
Greatness7 <Greatness7@gmail.com>
2023-12-08 22:49:43 -0800

    fix readthedocs building + update dependencies

--- conf.py
+++ conf.py
@@ -6,21 +6,22 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Blender Morrowind Plugin'
-copyright = '2020, GNU General Public License (GPL)'
-author = 'Greatness7'
+project = "Blender Morrowind Plugin"
+copyright = "2020, GNU General Public License (GPL)"
+author = "Greatness7"
 
 
 # -- General configuration ---------------------------------------------------
 
 # https://github.com/readthedocs/readthedocs.org/issues/2569
-master_doc = 'index'
+master_doc = "index"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autosectionlabel',
+    "sphinx_rtd_theme",
+    "sphinx.ext.autosectionlabel",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -29,20 +30,20 @@ extensions = [
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "venv"]
 
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
-html_logo = '_static/logo.png'
+html_theme = "sphinx_rtd_theme"
+html_logo = "_static/logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Theme options
 html_theme_options = {
